f6e2f6b9e5e6394487307415dddee0da3f51e283
unknown <agungarifr@gmail.com>
2025-03-19 13:49:25 +0700

    1st upload part4

---
+++ 1.js
@@ -0,0 +1,59 @@
+//membuat variable
+var a = 'a'
+let b = 'b'
+const c = 'c'
+
+//variable bisa nampung string, int, bool,array, object.
+
+let nama = 'cumi' //string
+let umur = 19 //int
+let isit = true //bool
+let duit = 123.4 //double?
+
+//operator
+
+let angka1 = 10
+let angka2 = 20
+console.log (angka1 - angka2,'min')
+console.log (angka1 + angka2,'tambah')
+console.log (angka1 * angka2,'kali')
+console.log (angka1 / angka2,'bagis')
+console.log (angka1 % angka2,'persen')
+
+let basketpanen = angka1*angka2
+console.log(basketpanen)
+
+basketpanen = angka1/angka2
+console.log(basketpanen)
+
+basketpanen = angka1-angka2
+console.log(basketpanen)
+
+basketpanen = angka1+angka2
+console.log(basketpanen)
+
+//string bisa pake + tapi gak bisa dikurang bagi kali dll
+
+let namadepan = 'agus'
+let namabelakang = 'sedih'
+let namalengkap = namadepan + ' ' + namabelakang
+
+console.log(namalengkap)
+console.log(namadepan)
+console.log(namabelakang)
+
+//cara baca code: dari kiri ke kanan lallu atas bawah
+//contoh
+
+let number = 33
+console.log(number+99) //132
+number = 13
+console.log(number + 10) //23
+number = false
+console.log(number + 10) //false = o, true =1 (bool)
+
+console.log(true+number+11) //12 ->  (bool) 1+0+ (int)11
+
+//tips teliti, spasi, spec char etc
+
+
