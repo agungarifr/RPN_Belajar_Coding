230bb876e7ef8847e819aed384b1e5bce61bac7e
unknown <agungarifr@gmail.com>
2025-03-20 11:16:13 +0700

    pakeswitch

--- part5/soal2.js
+++ part5/soal2.js
@@ -13,9 +13,10 @@
 //dan mulailah membaca dan mencontek di stackoverflow xixixi
 
 let tanggal = 20; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-let bulan = 11; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+let bulan = 3; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 let tahun = 1969; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
-
+let teksbulan;
+let ddmmyy;
 //code switch case kamu disini
 /* algo: fokus ke bulan dimana bulan 1 = januari dst 12 desember. meaning 1 refer to jan meaning using array
 1. declare array [Januari, Februari, Maret, April, Mei, Juni, Juli, Agustus, September, Oktober, November, Desember, Ophiucus]
@@ -23,9 +24,59 @@ let tahun = 1969; // assign nilai variabel tahun disini! (dengan angka antara 19
 3. Gabungin value dari masing masing variable jadi format tanggal
 */
 
-const arraybulan =["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];// declare array 
+/*
+const arraybulan =["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember","Desuari"];// declare array 
 const indexbulan = bulan -1;//array mulai dr 0
 
 const ddmmyy = tanggal+" "+arraybulan[indexbulan]+" "+tahun;
 console.log(ddmmyy);
+*/
 
+
+switch(bulan) {
+    case 1:
+        teksbulan = "January";
+      break;
+    case 2:
+        teksbulan = "Februari";
+        break;
+    case 3:
+        teksbulan = "Maret";
+        break;
+    case 4:
+        teksbulan = "April";
+        break;
+    case 5:
+        teksbulan = "Mei";
+        break;
+    case 6:
+        teksbulan = "Juni"
+        break;
+    case 7:
+        teksbulan = "Juli"
+        break;
+    case 8:
+        teksbulan = "Agustus"
+        break;
+    case 9:
+        teksbulan = "September"
+        break;
+    case 10:
+        teksbulan = "Oktober"
+        break;
+    case 11:
+        teksbulan = "November"
+        break;
+    case 12:
+        teksbulan = "Desember"
+        break;
+        default:
+            teksbulan = "cuman ada bulan 1 - 12 bang"
+}
+
+if (!teksbulan || teksbulan == "cuman ada bulan 1 - 12 bang" ) {
+    console.log("cuman ada bulan 1 - 12 bang");
+    
+} else ddmmyy = (`${tanggal} ${teksbulan} ${tahun}`); 
+
+console.log(ddmmyy);
